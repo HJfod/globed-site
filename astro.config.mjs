dbcaf6c4cc2437717741f20431c6ce480477ca42
HJfod <60038575+HJfod@users.noreply.github.com>
2024-03-05 00:20:09 +0200

    this should fix the url issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://globed.dev',
+    base: '/',
     integrations: [tailwind()]
 });
