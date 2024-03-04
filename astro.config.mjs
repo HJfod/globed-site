fa304581a76345166285f9af6eabd3148c094b69
HJfod <60038575+HJfod@users.noreply.github.com>
2024-03-04 23:56:57 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hjfod.github.io',
-    base: '/globed-site',
+    site: 'https://globed.dev',
+    base: '/',
     integrations: [tailwind()]
 });
