0c0960866cf03c6649beb0b0e2c36d0f41e7e0de
HJfod <60038575+HJfod@users.noreply.github.com>
2024-03-04 15:54:57 +0200

    add github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: 'https://hjfod.github.io',
+    base: '/globed-site',
+    integrations: [tailwind()]
 });
